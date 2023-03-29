3ac65f754d74c897be11c483e9e8cfc45ffb049d
NiziulLuizin <74280280+NiziulLuizin@users.noreply.github.com>
2023-03-29 16:52:01 -0700

    📝 adding the codes from version v0.1.0.0. (#1)

---
+++ Waypoint-marked-with-laser/setup-manager/SettingColor.cs
@@ -0,0 +1,9 @@
+﻿using System.Drawing;
+
+namespace Waypoint_marked_with_laser.setup_manager
+{
+    struct SettingColor
+    {
+        internal static Color ColorOfTheLaserBeam { get; set; }
+    }
+}
